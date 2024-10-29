8c8a9ff6ab74e9484111dd1a114823f37414f027
lau chun nam <D1114181063@gm.lhu.tw>
2024-10-29 12:00:58 +0800

    10/29

--- database/seeders/environmental_volunteersTableSeeder.php
+++ database/seeders/environmental_volunteersTableSeeder.php
@@ -4,15 +4,15 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class environmental_volunteersTableSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(environmental_volunteersTableSeeder);
+        //
     }
 }
