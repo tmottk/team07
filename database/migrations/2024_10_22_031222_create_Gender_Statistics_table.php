82fdc3dda56e6f7e9ac278aa4ac35bae785deee5
F313-060\user <D111418043@gm.lhu.edu.tw>
2024-11-05 11:49:58 +0800

    11/05課程進度

--- database/migrations/2024_10_22_031222_create_Gender_Statistics_table.php
+++ database/migrations/2024_10_22_031222_create_Gender_Statistics_table.php
@@ -17,9 +17,9 @@ class CreateGenderStatisticsTable extends Migration
             $table->id();
             $table->integer('year')->comment('年'); // 年
             $table->string('item')->comment('項目'); // 項目
-            $table->integer('total_count')->comment('總數');
-            $table->double('male_count')->comment('男性人數');
-            $table->double('female_count')->comment('女性人數');
+            $table->integer('total_count')->nullable()->comment('總數');
+            $table->double('male_count')->nullable()->comment('男性人數');
+            $table->double('female_count')->nullable()->comment('女性人數');
             $table->timestamps(); // created_at 和 updated_at 欄位
         });
     }
--- database/seeders/GenderStatisticsTableSeeder.php
+++ database/seeders/GenderStatisticsTableSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class GenderStatisticsTableSeeder extends Seeder
 {
@@ -11,8 +13,52 @@ class GenderStatisticsTableSeeder extends Seeder
      *
      * @return void
      */
+    public function generateRandomString($length = 10){
+        $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+    public function generateRandomName() {
+        $first_name = $this->generateRandomString(rand(2, 15));
+        $first_name = strtolower($first_name);
+        $first_name = ucfirst($first_name);
+        return $first_name;;
+    }
+
+    public function generateMaleFemaleCount($total_count) {
+        $male_count = rand(0, $total_count);
+        $female_count = $total_count - $male_count;
+    
+        return [
+            'male_count' => $male_count,
+            'female_count' => $female_count
+        ];
+    }
+    
     public function run()
     {
-        //
+
+        for ($i=0; $i<500; $i++)
+        {
+            $name=$this->generateRandomName();
+            $total_count = rand(100, 1000);
+            $gender_counts = $this->generateMaleFemaleCount($total_count);
+            $random_datetime = Carbon::now()->subMinutes(rand(1, 55));
+            $male_count = $gender_counts['male_count'];
+            $female_count = $gender_counts['female_count'];
+            \DB::table("gender_statistics")->insert([
+                'year'=>rand(98,111),
+                'item'=>$name,
+                'total_count' => $total_count,
+                'male_count' => $male_count, 
+                'female_count' => $female_count,
+                'created_at' => $random_datetime,
+                'updated_at' => $random_datetime
+            ]);
+      }
     }
 }
